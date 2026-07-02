b9a8b1237f39513986f5747cf64b87e01bfb1075
Arthaeris <the.shou7@googlemail.com>
2026-07-02 01:33:39 -0300

    Update wilds-json-data.js

--- wilds-json-data.js
+++ wilds-json-data.js
@@ -49,25 +49,42 @@ function addJsonNameIndex(map, item) {
 function buildJsonIndexes() {
   JSON_INDEX.itemByName.clear();
   JSON_INDEX.skillByName.clear();
+  JSON_INDEX.skillByGameId.clear();
   JSON_INDEX.monsterByName.clear();
   JSON_INDEX.armorByName.clear();
   JSON_INDEX.accessoryByName.clear();
   JSON_INDEX.charmByName.clear();
   JSON_INDEX.amuletByName.clear();
 
-  for (const item of JSON_DATA.item || []) addJsonNameIndex(JSON_INDEX.itemByName, item);
-  for (const item of JSON_DATA.skill || []) addJsonNameIndex(JSON_INDEX.skillByName, item);
+  for (const item of JSON_DATA.item || []) {
+    addJsonNameIndex(JSON_INDEX.itemByName, item);
+  }
+
+  for (const skill of JSON_DATA.skill || []) {
+    addJsonNameIndex(JSON_INDEX.skillByName, skill);
+
+    if (skill.game_id !== undefined) {
+      JSON_INDEX.skillByGameId.set(String(skill.game_id), skill);
+    }
+  }
+
   for (const item of JSON_DATA.largemonsters || []) addJsonNameIndex(JSON_INDEX.monsterByName, item);
   for (const item of JSON_DATA.armor || []) addJsonNameIndex(JSON_INDEX.armorByName, item);
   for (const item of JSON_DATA.accessory || []) addJsonNameIndex(JSON_INDEX.accessoryByName, item);
   for (const item of JSON_DATA.charm || []) addJsonNameIndex(JSON_INDEX.charmByName, item);
+
   for (const amulet of JSON_DATA.amulet || []) {
-  for (const rank of amulet.ranks || []) {
-    addJsonNameIndex(JSON_INDEX.amuletByName, {
-      ...rank,
-      parent_game_id: amulet.game_id,
-      is_random: amulet.is_random
-    });
+    for (const rank of amulet.ranks || []) {
+      addJsonNameIndex(JSON_INDEX.amuletByName, {
+        ...rank,
+        parent_game_id: amulet.game_id,
+        is_random: amulet.is_random
+      });
+    }
   }
 }
-}
+
+
+
+
+skillByGameId: new Map()
