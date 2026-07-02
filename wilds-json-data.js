c50e4cfb312fd2b7e067c2552f973c582c69274f
Arthaeris <the.shou7@googlemail.com>
2026-07-01 23:02:26 -0300

    Update wilds-json-data.js

--- wilds-json-data.js
+++ wilds-json-data.js
@@ -1,1 +1,25 @@
-.
+const JSON_DATA = {};
+
+async function loadJsonDatabase() {
+  const files = [
+    "accessory",
+    "amulet",
+    "armor",
+    "armorupgrade",
+    "charm",
+    "item",
+    "largemonsters",
+    "partnames",
+    "skill",
+    "species",
+    "stage",
+    "weaponseries"
+  ];
+
+  await Promise.all(
+    files.map(async file => {
+      const response = await fetch(`./json/${file}.json.txt`);
+      JSON_DATA[file] = await response.json();
+    })
+  );
+}
